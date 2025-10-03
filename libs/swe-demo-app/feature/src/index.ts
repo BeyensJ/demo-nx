1606f78106fa9d4689ce64c619b973e71494102d
jbeyens <u0157807@thomasmore.be>
2025-10-03 13:06:17 +0200

    Initial commit bis

---
+++ libs/swe-demo-app/feature/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/swe-demo-app-feature/swe-demo-app-feature';
+export * from "./lib/navbar-container/navbar-container";
---
+++ libs/swe-demo-app/ui/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/swe-demo-app-ui/swe-demo-app-ui';
+export * from "./lib/navbar/navbar";
